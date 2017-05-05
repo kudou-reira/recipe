ca0b9c06f0b51897a6afbe9fa7c05b2244a69b49
Stephen Mayeux <stephenmayeux@gmail.com>
2017-05-04 22:01:10 -0500

    formatting

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom';
 
 import FoodList from './components/food_list';
 
-
 const App = () => {
-    
-        return (
-            <div>
-                <div className = "recipeTitle">Recipe List!</div>
-                <FoodList />
-            </div>
-        );
+  return (
+    <div>
+      {/* Why not a header tag? */}
+      <div className="recipeTitle">Recipe List!</div>
+      <FoodList />
+    </div>
+  );
 }
 
 
