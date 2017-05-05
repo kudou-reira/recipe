bf3f8a07740d02477860863a377a7b09a28751c7
Stephen Mayeux <stephenmayeux@gmail.com>
2017-05-04 22:37:00 -0500

    finish formatting

--- src/components/list_ingredients.js
+++ src/components/list_ingredients.js
@@ -1,42 +1,18 @@
 import React from 'react';
 
 const IngredientProperties = (props) => {
-    
-    //split array
-    let ingArr = [];
-    ingArr = props.showList.ingredients.split(',');
-    //okay this works...props get passed
-    console.log(ingArr);
-    
-    
-    const ingrItems = ingArr.map((ingr, index) => {
-        
-        return(
-            
-            //add key  
-            <div>
-                <li key = {index} className = "list-group-item">
-
-                    {ingr}
-
-                </li>
-            </div>
-                                          
-        );
-        
-    });
-    
-    
-
-    return(
-        
-        <ul>
-            {ingrItems}
-        </ul>
-    
+  const ingredients = props.showList.split(',').map((ingredient, index) => {
+    return (
+      <div>
+        <li key={index} className="list-group-item">
+          {ingredient}
+        </li>
+      </div>
     );
-    
-    
+  });
+
+  return <ul>{ingredients}</ul>;
+
 }
 
 export default IngredientProperties;
--- src/components/show_modal.js
+++ src/components/show_modal.js
@@ -1,72 +1,52 @@
 import React from 'react';
 import IngredientProperties from './list_ingredients';
 
-
 const Example = (props) => {
 
-        console.log("hi");
-        const food1 = props.showFood.food;
-        const ingred1 = props.showFood.ingredients;
-        const placeHold = props.showFood;
-    
-        const editFood = '';
-    
-        return (
-            
-            <div className ="container">
-              
-              <button type="button" className ="btn btn-info" data-toggle="modal" data-target="#myModal">{food1}</button>
+  const { food, ingredients } = props.showFood
 
-
-              <div className ="modal modal-transparent fade" id="myModal" role="dialog">
-                <div className ="modal-dialog">
-
-                  <div className ="modal-content">
-                    <div className ="modal-header">
-                      <button type="button" className ="close">Ingredients</button>
-                      <h4 className="modal-title">{food1}</h4>
-                    
-                    </div>
-                    <div className ="modal-body">
-                      <p><IngredientProperties showList = {placeHold} /></p>
-                      {console.log(ingred1)}
-                    </div>
-
-                    
-                    <div id= "allFood" className = "collapse" data-toggle = "false">
-                        <div className = "centerNow">
-                            <input className = "form-control input-lg" type = "text">
-                                <textarea rows="2" cols="50" defaultValue = {ingred1}>
-                                </textarea>
-                            </input>
-                        </div>
-                    </div>
-
-                    <br />
-                    
-
-                    <div className ="modal-footer">
-                    
-                    <br />
-                    <button type="button" className ="btn btn-default" data-toggle="collapse" data-target="#allFood">Edit</button>
-                      
-                      
-                      <button type="button" className ="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
-                    
-                  </div>
-
-                </div>
-              </div>
-
+  return (
+    <div className="container">
+      <button
+        type="button"
+        className="btn btn-info"
+        data-toggle="modal"
+        data-target="#myModal"
+      >
+        {food}
+      </button>
+      <div className="modal modal-transparent fade" id="myModal" role="dialog">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close">Ingredients</button>
+              <h4 className="modal-title">{food}</h4>
             </div>
-            
+            <div className="modal-body">
+              <p>
+                <IngredientProperties showList={ingredients} />
+              </p>
+            </div>
+            <div id="allFood" className="collapse" data-toggle="false">
+              <div className="centerNow">
+                <input className="form-control input-lg" type="text">
+                  <textarea rows="2" cols="50" defaultValue={ingredients}></textarea>
+                </input>
+              </div>
+            </div>
+            {/* Use CSS styling margins/padding etc instead of <br> tags */}
+            <br/>
+            <div className="modal-footer">
+              <br/>
+              <button type="button" className="btn btn-default" data-toggle="collapse" data-target="#allFood">Edit</button>
+              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 
-       );
-        
-
-    
 }
 
 export default Example;
-
